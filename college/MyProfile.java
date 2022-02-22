e248d4f9494171bbfc054427f5f9267c290b718e
Boozer004 <96001367+Boozer004@users.noreply.github.com>
2022-02-22 10:24:26 +0530

    Update MyProfile.java

--- college/MyProfile.java
+++ college/MyProfile.java
@@ -2,10 +2,10 @@ package org.college;
 
 public class MyProfile extends MyScore {
 	public void myId() {
-		System.out.println("my register number is 2017506560");
+		System.out.println("my Register number is 310517104033");
 	}
 
 	public void myName() {
-		System.out.println("My name is Mubasheer G");
+		System.out.println("My name is Dharani Kumar T");
 	}
 }
