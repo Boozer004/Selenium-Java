a8c832554818c0d42b1a46f3f614afbd258e292c
Boozer004 <96001367+Boozer004@users.noreply.github.com>
2022-02-22 10:12:32 +0530

    Update PC.java

--- computer/PC.java
+++ computer/PC.java
@@ -2,15 +2,11 @@ package org.computer;
 
 public class PC extends Laptop {
 	public void getSystemInfo() {
-		System.out.println("The OS of PC is iOS");
+		System.out.println("The PC is working in iOS");
 	}
 
 	public static void main(String[] args) {
 		PC pc = new PC();
 		pc.getSystemInfo();
-
-	// used to call the method in laptop class	
-	//	Laptop laptop = new Laptop();
-	//	laptop.getSystemInfo();
 	}
 }
