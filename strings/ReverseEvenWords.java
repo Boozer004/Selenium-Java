5e3fd53480c643bdc7b6b51715fc4e7f5f71651d
Boozer004 <96001367+Boozer004@users.noreply.github.com>
2022-02-28 09:45:25 +0530

    Add files via upload

---
+++ strings/ReverseEvenWords.java
@@ -0,0 +1,14 @@
+package week2.day2;
+
+public class ReverseEvenWords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String input ="I am a software tester";
+//output: I ma a erawtfos tester
+String[] words = input.split(" ");
+
+for (int i=0; i<words.length;i++) {
+	}
+	}
+}
